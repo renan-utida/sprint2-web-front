912f28bb161deb230611e4b7b2d67612893b5e3f
Murilo <murilojusti2006@gmail.com>
2024-10-29 09:20:45 -0300

    Página mais informaçoes

--- src/routes/MaisInfo/BoxMaisInfo.jsx
+++ src/routes/MaisInfo/BoxMaisInfo.jsx
@@ -1,27 +1,85 @@
-import {} from "./maisInfo-styled"
+
+import iconBrasil from "../../assets/images/iconBrasil.png"
+import iconDistancia from "../../assets/images/iconDistancia.png"
+import iconPin from "../../assets/images/iconPin.png"
+import iconTemperatura from "../../assets/images/iconTemperatura.png"
+import iconUmidade from "../../assets/images/iconUmidade.png"
+import iconVelocidade from "../../assets/images/iconVelocidade.png"
+import iconPiloto from "../../assets/images/iconPiloto.png"
+import iconPiloto2 from "../../assets/images/iconPiloto2.png"
+import iconCarro from "../../assets/images/iconCarro.png"
+
+import { DivInfo, Icone } from "./maisInfo-styled"
 
 const BoxMaisInfo = () => {
     return(
         <main>
             <section>
-                <div>
-                    <img src="#" alt="Brasil" />
-                    <h1>Brasil</h1>
-                    <p>São Paulo</p>
-                    <p>07 Dez</p>
-                    <img src="#" alt="icone-local" />
-                    <p>Sambódromo do Anhembi</p>
-                    <img src="#" alt="temperatura" />
-                    <p>32^C</p>
-                    <img src="#" alt="umidade" />
-                    <p>45%</p>
-                </div>
+                <DivInfo>
+                    <img className='img-brasil' src={iconBrasil} alt="Brasil" />
+                    <div className="info">
+                        <div className="local-data">
+                            <h1>BRASIL</h1>
+                            <p>07 Dez</p>
+                        </div>
+                            <p>São Paulo</p>
+                        <div className="local">
+                            <Icone src={iconPin} alt="icone-local" />
+                            <p>Sambódromo do Anhembi</p>
+                        </div>
+                        <div className="temperatura">
+                            <Icone src={iconTemperatura} alt="temperatura" />
+                            <p>32ºC</p>
+                        </div>
+                        <div className="umidade">
+                            <Icone src={iconUmidade} alt="umidade" />
+                            <p>45%</p>
+                        </div>
+                    </div>
+
+                </DivInfo>
             </section>
-            <section>
-                <div>
-                    <h1>Telemetria dos veículos</h1>
+            <DivInfo>
+                <h1 className="titulo-telemetria">TELEMETRIA DOS VEÍCULOS</h1>
+                    <div className="card-piloto">
+                        <img src={iconPiloto} alt="piloto" />
+                        <div className="conteudo-card-piloto">
+                            <div className="nome-piloto">
+                                <h2>LUCAS</h2>
+                                <p>Di Grassi</p>
+                            </div>
+                            <div className="velocidade">
+                                <Icone src={iconVelocidade} alt="velocidade" />
+                                <p>140 km/h</p>
+                            </div>
+                            <div className="distancia">
+                                <Icone src={iconDistancia} alt="distância" />
+                                <p>3.5m</p>
+                            </div>
+                            <img src={iconCarro} alt="carro" />
+                        </div>
+                    </div>
+            </DivInfo>
+            <DivInfo>
+                <div className="card-piloto">
+                    <img src={iconPiloto2} alt="piloto" />
+                    <div className="conteudo-card-piloto">
+                        <div className="nome-piloto">
+                            <h2>SERGIO</h2>
+                            <p>Sette Camara</p>
+                        </div>
+                        <div className="velocidade">
+                            <Icone src={iconVelocidade} alt="velocidade" />
+                            <p>172 km/h</p>
+                        </div>
+                        <div className="distancia">
+                            <Icone src={iconDistancia} alt="distância" />
+                            <p>3m</p>
+                        </div>
+                        <img src={iconCarro} alt="carro" />
+                    </div>
                 </div>
-            </section>
+            </DivInfo>            
         </main>
     )   
 }
--- src/routes/MaisInfo/maisInfo-styled.js
+++ src/routes/MaisInfo/maisInfo-styled.js
@@ -2,5 +2,79 @@ import styled from "styled-components";
 
 export const MainMaisInfo = styled.section`
     min-height: 100vh;
-    margin-top: 75px
+    margin-top: 75px;
+`
+
+export const Icone = styled.img`
+    width: 32px;
+    height: 32px;
+`
+
+export const DivInfo = styled.div`
+    
+    margin-top: 50px;
+    margin-left: 100px;
+
+    .local-data{
+        display:flex;
+        gap: 75px;
+    }
+
+    .local{
+        display:flex;
+        gap: 10px;
+    }
+
+    .temperatura{
+        display:flex;
+        gap: 10px;
+    }
+
+    .umidade{
+        display:flex;
+        gap: 10px;
+    }
+
+    .velocidade{
+        display:flex;
+        gap: 10px;
+    }
+
+    .distancia{
+        display:flex;
+        gap: 10px;
+    }
+
+    .card-piloto{
+        display:flex;
+        margin-top: 50px;
+        gap: 120px;
+        background-color: #4646ff;
+        color: white;
+        border-radius: 50px;
+        width: 600px;
+        min-height: 300px
+    }
+
+    .conteudo-card-piloto{
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+        align-items: center;
+    }
+
+    .nome-piloto{
+        font-size: 25px;
+    }
+
+    .titulo-telemetria{
+        font-size: 35px;
+        margin: 50px;
+        text-decoration: underline;
+    }
+
+    .pilotos{
+        display: flex;
+    }
+
 `
