217a9b8d4d442c68952b5ea91878141436ea60e1
Murilo <murilojusti2006@gmail.com>
2024-10-30 14:31:30 -0300

    Finalização da estilização da página de informações

--- src/routes/MaisInfo/maisInfo-styled.js
+++ src/routes/MaisInfo/maisInfo-styled.js
@@ -1,81 +1,84 @@
 import styled from "styled-components";
-
+ 
 export const MainMaisInfo = styled.section`
   min-height: 100vh;
   margin-top: 75px;
   padding: 0 20px;
-
+  margin-bottom: 4rem;
+ 
   .titulo-telemetria {
     text-align: center;
     font-size: 3rem;
     text-decoration: underline;
   }
-
+ 
   @media (max-width: 768px) {
     .titulo-telemetria {
       font-size: 2rem;
     }
   }
 `;
-
+ 
 export const DivTelemetria = styled.div`
   display: flex;
   flex-direction: column;
   row-gap: 1rem;
   margin-top: 50px;
   padding: 0 20px;
-
+ 
+ 
   .localizacao {
     display: flex;
     align-items: center;
     flex-wrap: wrap;
   }
-
+ 
   .info-pista {
     display: flex;
     flex-direction: column;
     row-gap: 0.5rem;
   }
-
+ 
   .telemetria-info {
     display: flex;
     align-items: center;
   }
-
+ 
   @media (max-width: 768px) {
     margin-left: 20px;
   }
 `;
-
+ 
 export const Icone = styled.img`
   width: 32px;
   height: 32px;
 `;
-
+ 
 export const Runners = styled.section`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   gap: 20px;
   padding: 0 10px;
-
+ 
+ 
   @media (max-width: 1024px) {
     grid-template-columns: repeat(2, 1fr);
   }
-
+ 
   @media (max-width: 768px) {
     grid-template-columns: 1fr;
   }
 `;
-
+ 
 export const DivInfo = styled.div`
   margin-top: 50px;
   padding: 0 20px;
-
+ 
   .local-data {
     display: flex;
     gap: 75px;
   }
-
+ 
   .local,
   .temperatura,
   .umidade,
@@ -84,7 +87,7 @@ export const DivInfo = styled.div`
     display: flex;
     gap: 10px;
   }
-
+ 
   .card-piloto {
     display: flex;
     flex-direction: column;
@@ -94,30 +97,29 @@ export const DivInfo = styled.div`
     color: white;
     border-radius: 20px;
     padding: 20px;
-    width: 100%;
     max-width: 100%;
     min-height: 300px;
   }
-
+ 
   .conteudo-card-piloto {
     display: flex;
     flex-direction: column;
     align-items: center;
   }
-
+ 
   .nome-piloto {
     font-size: 2rem;
   }
-
+ 
   .pilotos {
     display: flex;
   }
-
+ 
   .img-carro {
-    width: 140px;
-    height: auto;
+    width: 160px;
+    height: 50px;
   }
-
+ 
   @media (max-width: 768px) {
     .nome-piloto {
       font-size: 1.5rem;
